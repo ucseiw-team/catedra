635e7773aee54dc8a92a56ec453efc5ebb5977dc
Juan Pedro Fisanotti <fisadev@gmail.com>
2014-04-14 21:32:50 -0300

    Agregados archivos faltantes de los contadores ajax

---
+++ ejemplo_django/noticias/sitio/static/contador_ajax.js
@@ -0,0 +1,22 @@
+
+function contador() {
+  /* ajax usando un template como resultado */
+  p_contador = $('#contador');
+  p_contador.load('/sumar_numero/' + p_contador.text() + '/');
+
+  /* ajax usando datos en json como resultado */
+  p_contador_json = $('#contador_json');
+  $.ajax({
+      "url": '/sumar_numero_json/' + p_contador_json.text() + '/',
+      "dataType": "json"
+  }).done(
+    function (result) {
+      p_contador_json.text(result.numero.toString());
+      console.log(result);
+      debugger;
+  });
+  
+  setTimeout(contador, 1000);
+}
+
+setTimeout(contador, 1000);
