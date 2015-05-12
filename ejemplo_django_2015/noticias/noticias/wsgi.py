29e0d4725abb0d303206f7026386527855ceb650
Juan Pedro Fisanotti <fisadev@gmail.com>
2015-05-12 15:25:21 -0300

    Wsgi actualizado para heroku

--- ejemplo_django_2015/noticias/noticias/wsgi.py
+++ ejemplo_django_2015/noticias/noticias/wsgi.py
@@ -11,4 +11,9 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noticias.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+if os.environ.get('HEROKU', False):
+    from dj_static import Cling
+    application = Cling(get_wsgi_application())
+else:
+    application = get_wsgi_application()
