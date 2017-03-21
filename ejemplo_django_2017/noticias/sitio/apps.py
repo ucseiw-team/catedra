c3b84b08ac7920d9456ad250cb99d35b56f01408
Juan Pedro Fisanotti <fisadev@gmail.com>
2017-03-20 22:06:06 -0300

    Added django 2017 example

---
+++ ejemplo_django_2017/noticias/sitio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SitioConfig(AppConfig):
+    name = 'sitio'
--- ejemplo_twitter_2015/twitear.py
+++ ejemplo_twitter_2015/twitear.py
@@ -12,3 +12,4 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 tweets = api.user_timeline('fedeZurbriggen', count=10, page=1)
+print tweets[0]
