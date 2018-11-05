727587c550196d72c806462c2fa8391cc226e115
mfferrero1 <mariano-ferrero@outlook.com>
2018-11-05 00:17:13 -0300

    Add web api controller

---
+++ ejemplo_net/Comunidad/Comunidad/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using RepositorioClases;
+using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Comunidad.Controllers
+{
+    public class UsersController : ApiController
+    {
+        public IHttpActionResult Get(int id)
+        {
+            var user = UserService.Get(id).Select(u => new User()
+            {
+                Email = u.Email,
+                Id = u.Id,
+                Name = u.Name,
+                Password = u.Password,
+                IdRol = u.IdRol
+            }).FirstOrDefault();
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(User user)
+        {
+            UserService.Create(new RepositorioClases.User()
+            {
+                Email = user.Email,
+                Name = user.Name,
+                Password = user.Password,
+                IdRol = user.IdRol
+            });
+
+            return Ok();
+        }
+    }
+}
