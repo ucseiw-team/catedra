fb8a6c56b8b5ac359d2c33712e2c7d158fc0996b
Juan Pedro Fisanotti <fisadev@gmail.com>
2020-09-29 18:08:55 -0300

    Simpler web app code

--- ejemplo_docker_actual/web.py
+++ ejemplo_docker_actual/web.py
@@ -1,27 +1,29 @@
 from datetime import datetime
+from pathlib import Path
+
 from flask import Flask
 
 
 app = Flask(__name__)
 start = datetime.now()
 
+things_path = Path('./data/things.txt')
+
 
 @app.route("/")
 def home_page():
-    return "<h1>Demo web page (server started at {start})</h1>".format(start=start)
+    return f"<h1>Demo web page (server started at {start})</h1>"
 
 
 @app.route("/save/<thing>/")
 def save(thing):
-    with open('./data/things.txt', 'a') as things_file:
-        things_file.write("{thing} at {time}\n".format(thing=thing, time=datetime.now()))
-    return "<h1>Saved: {thing}</h1>".format(thing=thing)
+    with things_path.open('a') as things_file:
+        things_file.write(f"saved {thing} at {datetime.now()}\n")
+
+    return f"<h1>Saved: {thing}</h1>"
 
 
 @app.route("/read/")
 def read():
-    with open('./data/things.txt') as things_file:
-        things = list(things_file.readlines())
-    things_as_html = ''.join("<p>{thing}</p>".format(thing=thing)
-                             for thing in things)
-    return "<h1>Current things: {things}</h1>".format(things=things_as_html)
+    things = things_path.read_text()
+    return f"<h1>Current things:</h1><pre>{things}</pre>"
