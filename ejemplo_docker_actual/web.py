7d76e96dfc32a2ca817d170c075f04df481fc823
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-10-08 12:52:31 -0300

    Simplified example and update python version

--- ejemplo_docker_actual/web.py
+++ ejemplo_docker_actual/web.py
@@ -1,35 +1,31 @@
-from datetime import datetime
+from random import randint
 from pathlib import Path
 
 from flask import Flask
 
 
 app = Flask(__name__)
-start = datetime.now()
+server_id = randint(0, 1000000)
 
 things_path = Path('./data/things.txt')
 
 
 @app.route("/")
 def home_page():
-    return f"<h1>Demo web page (server started at {start})</h1>"
+    try:
+        things = things_path.read_text()
+    except:
+        things = "No things saved!"
+
+    return f"<h1>Demo home page</h1><h3>Server {server_id}</h3><p>Current things:</p><pre>{things}</pre>"
 
 
 @app.route("/save/<thing>/")
 def save(thing):
     with things_path.open('a') as things_file:
-        things_file.write(f"saved {thing} at {datetime.now()}\n")
+        things_file.write(f"saved {thing} from server {server_id}\n")
 
-    return f"<h1>Saved: {thing}</h1>"
-
-
-@app.route("/read/")
-def read():
-    try:
-        things = things_path.read_text()
-    except:
-        things = "No things saved!"
-    return f"<h1>Current things:</h1><pre>{things}</pre>"
+    return f"<h1>Saved: {thing}</h1><h3>Server {server_id}</h3>"
 
 
 app.run(host="0.0.0.0")
